2b728004e414bf2ac61407ce841fa24867522694
bill <bill.mill@gmail.com>
2010-10-23 22:15:18 -0400

    adding a file for my code

---
+++ source/grafico.parcoords.js
@@ -0,0 +1,14 @@
+/**
+ * Grafico - SVG graphing library - parallel coordinates chart file
+ *
+ * Copyright (c) 2009 - 2010 Kilian Valkhof (kilianvalkhof.com) - Originally developed by Alex Young (http://alexyoung.org)
+ * This file originally developed by Bill Mill (http://billmill.org bill.mill@gmail.com)
+ *
+ * Visit grafico.kilianvalkhof.com for more information and changelogs.
+ * Licensed under the MIT license. http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+"use strict";
+Grafico.ParallelCoordinatesGraph = Class.create(Grafico.BaseGraph, {
+});
